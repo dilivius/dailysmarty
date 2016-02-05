b6b6bc39f43c24303c3d2bd6e635cfe186fa18a5
Jordan Hudgens <jordan@wow.com>
2016-02-05 16:09:52 -0600

    Added initial auth implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,108 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705042009) do
+ActiveRecord::Schema.define(version: 20160205214837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.text     "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.boolean  "authorized"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "post_likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "post_likes", ["post_id"], name: "index_post_likes_on_post_id", using: :btree
-  add_index "post_likes", ["user_id"], name: "index_post_likes_on_user_id", using: :btree
-
-  create_table "post_tags", force: :cascade do |t|
-    t.integer  "post_id"
-    t.string   "title",      limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "post_tags", ["post_id"], name: "index_post_tags_on_post_id", using: :btree
-
-  create_table "posts", force: :cascade do |t|
-    t.integer  "topic_id"
-    t.integer  "user_id"
-    t.string   "title",      limit: 255
-    t.text     "message"
-    t.integer  "updates"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id", using: :btree
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
-
-  create_table "topicizations", force: :cascade do |t|
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "post_id"
-  end
-
-  add_index "topicizations", ["post_id"], name: "index_topicizations_on_post_id", using: :btree
-  add_index "topicizations", ["topic_id"], name: "index_topicizations_on_topic_id", using: :btree
-
-  create_table "topics", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "position"
-    t.integer  "category_id"
-  end
-
-  add_index "topics", ["category_id"], name: "index_topics_on_category_id", using: :btree
-
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "username",               limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "avatar"
+    t.string   "username"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
-  add_foreign_key "topicizations", "posts"
-  add_foreign_key "topicizations", "topics"
 end
