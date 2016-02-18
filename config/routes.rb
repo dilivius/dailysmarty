bff761b44de10e8e98ba4405f33d5f4de0230b2c
Jordan Hudgens <jordan@wow.com>
2016-02-17 18:14:39 -0600

    Implemented nested route feature for posts under topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :topics
+  resources :topics do
+  	scope module: :topics do
+  		resources :posts
+  	end
+  end
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'static#home'
 end
