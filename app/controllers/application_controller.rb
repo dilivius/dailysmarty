2e799c670ceb0220fc60d3eaa0a375bfe548c616
Jordan Hudgens <jordan@wow.com>
2016-07-11 14:38:58 -0500

    Implemented ability to capture subdomains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  before_action :get_subdomain
+
   def current_user
     super || OpenStruct.new(full_name: 'Guest')
   end
@@ -13,5 +15,11 @@ class ApplicationController < ActionController::Base
     def user_not_authorized
       flash[:alert] = "You are not authorized to access this page."
       redirect_to(request.referrer || root_path)
+    end
+
+    def get_subdomain
+      puts "The Subdomain is:"
+      puts request.subdomain
+      raise
     end
 end
